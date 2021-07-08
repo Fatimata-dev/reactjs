158e0d68c7614311e8fdf2dbb303d262440acfdb
Fatimata-dev <dramefatim7@gmail.com>
2021-07-08 16:56:37 +0200

    react router

---
+++ 13-create-react-app/src/component/Navigation.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navigation.css';
+
+function Navigation(props) {
+    return (
+        <nav>
+            {/*
+                Par defaut, les urls qui correspondent, auront la classe 'active'
+                on peut redefinir la classe a ajouter avec 
+                activeClassName="current"
+            */}
+            <NavLink exact to="/">
+                Accueil
+            </NavLink>
+            <NavLink to="/life-cycle">
+                Cycle de vie
+            </NavLink>
+            <NavLink to="/params/avatar">
+                Params
+            </NavLink>
+            <NavLink to="/back">
+                Retour
+            </NavLink>
+            <NavLink to="/login">
+                connexion
+            </NavLink>
+            <NavLink to="/admin">
+                Admin
+            </NavLink>
+        </nav>
+    );
+}
+export default Navigation;
